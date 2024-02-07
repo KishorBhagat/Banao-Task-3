5225dfbb6cfbf79d6a3945a740013f410505f4dc
Kishor Bhagat <kishorebhagat663@gmail.com>
2024-02-07 12:34:10 +0530

    feat: added all pages

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("script added");
+
+let currentPage = 1
+
+const carouselItem = document.querySelectorAll('.item');
+const carousel = document.querySelector('.carousel')
+
+// const dotClick = (page) => {
+//     console.log(page-1)
+//     carouselItem.forEach(item => {
+//         item.style.transform = `translateX(-${(page-1)*100}vw)`
+//       });
+// }
+
+// carousel.addEventListener('wheel', (event) => {
+//   const deltaY = event.deltaY;
+//   if(deltaY > 0) {
+//     if(currentPage < 7) {
+//         currentPage = currentPage + 1;
+//         dotClick(currentPage);
+//     }
+//   }
+//   else if (deltaY < 0) {
+//     if(currentPage > 1) {
+//         currentPage = currentPage - 1;
+//         dotClick(currentPage);
+//     }
+//   }
+//   console.log(currentPage)
+// });
