8ce7908e62bc21e23d91f37d489245c63edfebcb
Kishor Bhagat <kishorebhagat663@gmail.com>
2024-02-07 18:57:23 +0530

    feat: basic functionality achieved

--- script.js
+++ script.js
@@ -1,30 +1,76 @@
-console.log("script added");
-
-let currentPage = 1
-
+const carousel = document.querySelector('.carousel');
 const carouselItem = document.querySelectorAll('.item');
-const carousel = document.querySelector('.carousel')
 
-// const dotClick = (page) => {
-//     console.log(page-1)
-//     carouselItem.forEach(item => {
-//         item.style.transform = `translateX(-${(page-1)*100}vw)`
-//       });
-// }
+function dotClick(page, event) {
+    console.log(event)
+    owl.trigger("to.owl.carousel", page-1)
+}
 
-// carousel.addEventListener('wheel', (event) => {
-//   const deltaY = event.deltaY;
-//   if(deltaY > 0) {
-//     if(currentPage < 7) {
-//         currentPage = currentPage + 1;
-//         dotClick(currentPage);
+
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+    loop: !1,
+    nav: !1,
+    dots: !1,
+    mouseDrag: !1,
+    touchDrag: !0,
+    margin: 20,
+    stagePadding: 0,
+    transitionStyle: "ease",
+    animateIn: "easeIn",
+    slideTransition: "linear",
+    responsive: {
+        0: { items: 1 },
+        800: { items: 1 },
+        1200: { items: 1 }
+    }
+})
+owl.on("mousewheel", (function (e) {
+    e.deltaY < 0 ? owl.trigger("next.owl") : owl.trigger("prev.owl")
+    e.preventDefault()
+}))
+owl.on("to.owl.carousel", (function (e) {
+    // console.log("hii")
+}))
+
+
+const sections = gsap.utils.toArray('.carousel section');
+// console.log(sections)
+
+// let scrollTween = gsap.to(sections, {
+//     xPercent: -100 * (sections.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".carousel",
+//         pin: true,
+//         scrub: 1,
+//         end: () => "+=" + document.querySelector(".carousel").offsetWidth
 //     }
-//   }
-//   else if (deltaY < 0) {
-//     if(currentPage > 1) {
-//         currentPage = currentPage - 1;
-//         dotClick(currentPage);
-//     }
-//   }
-//   console.log(currentPage)
-// });
+// })
+
+let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".carousel",
+        pin: true,
+        scrub: 1,
+        end: () => "+=" + document.querySelector(".carousel").offsetWidth
+    }
+})
+sections.forEach(section => {
+    let up = section.querySelectorAll('.up');
+
+    gsap.from(up, {
+        y: -130,
+        opacity: 0,
+        duration: 2,
+        stagger: 0.1,
+        scrollTrigger: {
+            trigger: section,
+            containerAnimation: scrollTween,
+            start: "left center",
+            markers: true
+        }
+    })
+})
